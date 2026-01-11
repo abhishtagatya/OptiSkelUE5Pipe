bf377c2d59ce35410827322c97b08c64afe6230b
abhis <565105@mail.muni.cz>
2026-01-11 15:55:06 +0100

    Renaming Bones & Armature

--- Blender/skeletal_mesh_preparation.py
+++ Blender/skeletal_mesh_preparation.py
@@ -1,6 +1,6 @@
 import bpy
 
-PROGRAM_NAME = "OptiSkelUE5Pipe-Skeletal"
+PROGRAM_NAME = "OptiSkelUE5Pipe-SKMPrep"
 
 #region DATA
 SKP_CHAIN_MATERIAL = {
@@ -67,12 +67,17 @@ SKP_CHAIN_MATERIAL = {
 }
 #endregion
 
-class SkeletalMeshPlaceholder:
+class SkeletalMeshPreparation:
 
-    def __init__(self, mesh_primitive: str = "CUBE", mesh_size: float = 0.5, mesh_color_method: str = "CHAIN"):
+    def __init__(self, 
+                 mesh_primitive: str = "CUBE", 
+                 mesh_size: float = 0.5, 
+                 mesh_color_method: str = "CHAIN",
+                 armature_rename: bool = False):
         self.mesh_primitive = mesh_primitive
         self.mesh_size = mesh_size
         self.mesh_color_method = mesh_color_method
+        self.armature_rename = armature_rename
 
     def create_primitive_adapter(self):
         """
@@ -105,9 +110,9 @@ class SkeletalMeshPlaceholder:
         """
         Find the Bone's Chain Group
         """
-        bone_name = bone_name.split("_")
-        if len(bone_name) > 1:
-            bone_name = bone_name[1]
+        prefixes = bone_name.split("_")
+        if len(prefixes) > 1:
+            bone_name = prefixes[1]
 
         for key, value in SKP_CHAIN_MATERIAL.items():
             if any(bone_name.startswith(core_name) for core_name in value["bones"]):
@@ -148,30 +153,52 @@ class SkeletalMeshPlaceholder:
         Construct Material Name by Key
         """
         return f"M_{self.__class__.__name__}_{self.mesh_color_method}_{str(key)}"
+    
+    def rename_armature(self, part_name) -> str:
+        """
+        Rename an Armature Object Parts by Stripping Prefixes.
+        """
+        prefixes = part_name.split("_")
+        if len(prefixes) > 1:
+            return prefixes[1]
+        return part_name
 
-    def place_mesh_armature_bone_tip(self, selected_armature_name="Armature"):
+    def place_mesh_armature_bone_tip(self, selected_armature):
         """
         Place Primitives with Material on Armature's Bone's Tip Position
         """
-        selected_armature = bpy.data.objects.get(selected_armature_name)
 
+        # Check that Object is an Armature
         if selected_armature is None or selected_armature.type != "ARMATURE":
             print(PROGRAM_NAME + ": " + f"Couldn't find an Armature named {selected_armature_name}.")
             return
         
+        # Set to Object Mode
         if bpy.ops.object.mode_set.poll():
             if bpy.context.mode != "OBJECT":
                 bpy.ops.object.mode_set(mode="OBJECT")
 
         bpy.ops.object.select_all(action="DESELECT")
 
+        # Rename Armature Pose
+        if (self.armature_rename):
+            selected_armature.data.name = self.rename_armature(selected_armature.data.name)
+
+        # Iterate for All Bones in the Armature
         for pose_bone in selected_armature.pose.bones:
+
+            # Rename Bone (remove OptiTrack Prefix)
             bone_name = pose_bone.name
+            if (self.armature_rename):
+                pose_bone.name = self.rename_armature(pose_bone.name)
+                bone_name = pose_bone.name
             
+            # Create Primitive as Placeholder for UE5 SKM
             self.create_primitive_adapter()
             pobj = bpy.context.active_object
             pobj.name = f"P_{bone_name}"
             
+            # Place the Primitive to the Bone
             bone_world_matrix = selected_armature.matrix_world @ pose_bone.matrix
             pobj.matrix_world = bone_world_matrix
 
@@ -180,6 +207,7 @@ class SkeletalMeshPlaceholder:
             selected_armature.select_set(True)
             bpy.context.view_layer.objects.active = selected_armature
 
+            # Set Material based on SKM Chain for Primitive
             proxy_material = self.get_materials_by_method(bone_name)
 
             if pobj.data.materials:
@@ -187,6 +215,7 @@ class SkeletalMeshPlaceholder:
             else:
                 pobj.data.materials.append(proxy_material)
             
+            # Constraint place Primitive as a Child of the Bone
             pobj.parent = selected_armature
             pobj.parent_type = 'BONE'
             pobj.parent_bone = bone_name
@@ -212,14 +241,17 @@ class SkeletalMeshPlaceholder:
             
             print(PROGRAM_NAME + f": Created P_{bone_name} attached to {bone_name} of {selected_armature}.")
     
-    def run(self, selected_armature="Armature"):
+    def run(self, selected_armature):
         """
         Run the Skeletal Mesh Placeholder
         """
         print(PROGRAM_NAME + f": Running Skeletal Mesh Placeholder Program")
         self.create_materials_by_method()
         self.place_mesh_armature_bone_tip(selected_armature)
-    
+        if (self.armature_rename):
+            selected_armature.name = self.rename_armature(selected_armature.name)
+
 if __name__ == "__main__":
-    smp = SkeletalMeshPlaceholder(mesh_primitive="CUBE", mesh_size=2.0)
-    smp.run(selected_armature="Fitri_Hips")
+    smp = SkeletalMeshPreparation(mesh_primitive="CUBE", mesh_size=2.0, armature_rename=True)
+    for obj in bpy.context.selected_objects:
+        smp.run(selected_armature=obj)
